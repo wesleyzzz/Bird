e98751bbae2507d66731581c5b78be54ee91e5b2
moose <zuzupsilon@gmail.com>
2019-03-30 20:19:46 -0600

    test

--- include/base/BirdRevision.h
+++ include/base/BirdRevision.h
@@ -3,7 +3,7 @@
 #ifndef BIRD_REVISION_H
 #define BIRD_REVISION_H
 
-#define BIRD_REVISION "git commit 978e951 on 2019-03-30"
-#define BIRD_VERSION "978e951"
+#define BIRD_REVISION "git commit 93eefdd on 2019-03-30"
+#define BIRD_VERSION "93eefdd"
 
 #endif // BIRD_REVISION_H
